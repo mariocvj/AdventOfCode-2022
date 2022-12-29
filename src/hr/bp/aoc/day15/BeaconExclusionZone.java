8611800c21630ecc930a6fed7399015de32497d4
Mario Cvjetojević <mario.cvjetojevic@bitnaprednost.hr>
2022-12-29 12:58:03 +0100

    AOC-day15 part1 debugged & refactored

--- src/hr/bp/aoc/day15/BeaconExclusionZone.java
+++ src/hr/bp/aoc/day15/BeaconExclusionZone.java
@@ -12,10 +12,9 @@ import java.util.List;
 public class BeaconExclusionZone extends BaseDay {
 
     private List<SenzorBeaconPair> senzorBeaconPairs;
-    private Character scanedRow[];
-    private Integer areaSize;
-    private Integer scanedRowY;
-    private Integer scanedRowOffset;
+    private Character[] scanedRow;
+    private int scanedRowY;
+    private int scanedRowOffset;
 
     public class SenzorBeaconPair {
         Point senzor;
@@ -27,12 +26,12 @@ public class BeaconExclusionZone extends BaseDay {
 
         Point beacon;
 
-        protected Integer getManhattanDistance() {
+        protected int getManhattanDistance() {
             return Math.abs(senzor.x - beacon.x) + Math.abs(senzor.y - beacon.y);
         }
 
-        protected void scanPositionsInRowPartOne(Integer row) {
-            Integer scanWidth = Math.max(0, this.getManhattanDistance() - Math.abs(senzor.y - row) + 1);
+        protected void scanPositionsInRowPartOne(int row) {
+            int scanWidth = Math.max(0, this.getManhattanDistance() - Math.abs(senzor.y - row) + 1);
             System.out.println("scanWidth: " + scanWidth);
 
             for (int x = senzor.x - scanWidth; x < senzor.x + scanWidth - 1; x++) {
@@ -45,18 +44,27 @@ public class BeaconExclusionZone extends BaseDay {
 
     @Override
     protected String partOne(List<String> puzzleInputRowsList) {
-        Integer scanned = 0;
 
         initialiseSenzorBeaconPairs(puzzleInputRowsList);
 
         initialisePartOneScanedRow();
 
-        for(SenzorBeaconPair senzorBeacon: senzorBeaconPairs){
-            senzorBeacon.scanPositionsInRowPartOne(2000000);
+        for (SenzorBeaconPair senzorBeacon : senzorBeaconPairs) {
+            senzorBeacon.scanPositionsInRowPartOne(scanedRowY);
         }
 
+        return String.valueOf(getNoBeaconPositionCount());
+    }
 
-        return String.valueOf(scanned);
+    private int getNoBeaconPositionCount() {
+        int scanned = -1;
+
+        for (Character position : scanedRow) {
+            if (position.equals('X')) {
+                scanned++;
+            }
+        }
+        return scanned;
     }
 
     @Override
@@ -89,27 +97,43 @@ public class BeaconExclusionZone extends BaseDay {
     }
 
     private void initialiseSenzorBeaconPairs(List<String> puzzleInputRowsList) {
-        String inputNumbers[];
-        Point senzor;
-        Point beacon;
-        senzorBeaconPairs = new ArrayList<SenzorBeaconPair>();
+
+        SenzorBeaconPair pair;
+        senzorBeaconPairs = new ArrayList<>();
 
         for (String inputRow : puzzleInputRowsList) {
-            inputNumbers = inputRow.replaceAll("[[a-zA-Z] =]", "").split("[,:]");
 
-            senzor = new Point(Integer.parseInt(inputNumbers[0]), Integer.parseInt(inputNumbers[1]));
-            beacon = new Point(Integer.parseInt(inputNumbers[2]), Integer.parseInt(inputNumbers[3]));
+            pair = asSenzorBeaconPair(inputRow);
 
-            senzorBeaconPairs.add(new SenzorBeaconPair(senzor, beacon));
+            senzorBeaconPairs.add(pair);
 
-            if (scanedRowY != null) {
-                if (scanedRowY == senzor.y) {
-                    scanedRow[senzor.x + scanedRowOffset] = 'S';
-                }
-                if (scanedRowY == beacon.y) {
-                    scanedRow[beacon.x + scanedRowOffset] = 'B';
-                }
-            }
+            saveSenzorAndBeaconPositionForPartOne(pair);
+        }
+    }
+
+    private SenzorBeaconPair asSenzorBeaconPair(String row) {
+
+        String[] inputNumbers;
+        Point senzor;
+        Point beacon;
+
+        inputNumbers = row.replaceAll("[a-zA-Z\s=]", "").split("[,:]");
+
+        senzor = new Point(Integer.parseInt(inputNumbers[0]), Integer.parseInt(inputNumbers[1]));
+        beacon = new Point(Integer.parseInt(inputNumbers[2]), Integer.parseInt(inputNumbers[3]));
+
+        return new SenzorBeaconPair(senzor, beacon);
+    }
+
+
+    private void saveSenzorAndBeaconPositionForPartOne(SenzorBeaconPair position) {
+
+        if (scanedRowY == position.senzor.y) {
+            scanedRow[position.senzor.x + scanedRowOffset] = 'S';
+        }
+
+        if (scanedRowY == position.beacon.y) {
+            scanedRow[position.beacon.x + scanedRowOffset] = 'B';
         }
     }
 }
